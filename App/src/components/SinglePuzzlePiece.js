be1996cc16b2e6b6469471552e90ecd858b80e5e
Arek U <arkadiuszurbankiewicz@gmail.com>
2021-07-17 00:06:26 +0200

    add drag and drop events

--- App/src/components/SinglePuzzlePiece.js
+++ App/src/components/SinglePuzzlePiece.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React , { useState, useEffect, useCallback  } from 'react';
 
-function SinglePuzzlePiece({color}) {
+function SinglePuzzlePiece({color, startTop, startLeft}) {
+
+    const [position, setPosition] = useState({top: startTop, left: startLeft})
+    const [hover, setHover] = useState(false)
+
+    useEffect(() => {
+        console.log("no a jakkk")
+      }, []);
+
+      const event = useCallback((e) => 
+        {
+                console.log(`mouse move : ${e.offsetX} ${e.offsetY} `)   
+                setPosition({left: e.clientX -25, top: e.clientY -25})
+        },[])
+
+      useEffect(() => {
+       
+        if(hover)
+        {
+            console.log(hover)
+            document.addEventListener('mousemove', event);
+        }
+        else{
+            console.log(hover)
+            document.removeEventListener('mousemove', event);
+        }
+        },
+       [hover]);
+
     return (
     <>
-        <div className = {`playboard__singlepuzzlepiece playboard__singlepuzzlepiece--color-${color}`} >
+        <div onMouseDown = {()=> setHover(true)} onMouseUp= {()=> setHover(false)} style={{opacity:`${hover?0.5:1}`, top: position.top, left:position.left}} className = {`playboard__singlepuzzlepiece playboard__singlepuzzlepiece--color-${color}`} >
         </div>
     </>
     );
--- App/src/index.js
+++ App/src/index.js
@@ -10,12 +10,10 @@ import './resources/scss/index.scss';
 function App(props) {
     return (
     <div className= "playboard">
-      <SinglePuzzlePiece color = "red"/>
-      <SinglePuzzlePiece color = "green"/>
+      <SinglePuzzlePiece color = "red" startTop = {20} startLeft= {20}/>
+      <SinglePuzzlePiece color = "green" startTop = {20} startLeft= {20}/>
     </div>
     );
   }
-
-
 
 ReactDOM.render(<App name="Your name"/>, document.getElementById('app'));
