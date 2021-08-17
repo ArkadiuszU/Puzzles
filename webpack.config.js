d3a3d88b91addf6b77ac4c21f9c7b5ba179bd0a1
arkurb <arkadiuszurbankiewicz@gmail.com>
2021-08-17 21:31:05 +0200

    open browser

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,8 @@ module.exports = {
   // webpack 5 comes with devServer which loads in development mode
  devServer: {
    port: 3000,
-   watchContentBase: true
+   watchContentBase: true,
+   open: true
  },
   // Rules of how webpack will take our files, complie & bundle them for the browser 
  module: {
