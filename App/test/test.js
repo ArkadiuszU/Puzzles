17ec55736ceff3340ddc829ff1e29263f8fdafd6
Arkadiusz Urbankiewicz <Arkadiusz.Urbankiewicz@duni.com>
2021-09-15 09:30:24 +0200

    add jest test to project

---
+++ App/test/test.js
@@ -0,0 +1,11 @@
+
+// test('test', () => {
+//     const component = renderer.create(
+//       <Welcome/>,
+//     );
+//     let tree = component.toJSON();
+//     expect(tree).toMatchSnapshot();
+
+test("test", () => {
+  expect(true).toBe(true);
+}) 
